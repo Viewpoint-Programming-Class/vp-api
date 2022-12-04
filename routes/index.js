14fa3faad07192bf508b87b6c4e650f00f45b307
Shimon Brandsdorfer <sbrandsdorfer@viewpointlearn.com>
2022-12-04 14:34:20 -0500

    save

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
-router.use('/companies', require('./companies'))
+router.use('/companies', require('./companies'));
+router.use('/games', require('./games'));
 
 
-
-module.exports = router
+module.exports = router;
